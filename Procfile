d39a903e1c67f623145d1df4db0cf4ca8a955e0b
zobla <zobla.kv@gmail.com>
2020-06-26 18:00:36 +0200

    a

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: node server/app.js
--- server/app.js
+++ server/app.js
@@ -1,10 +1,1 @@
-const io = require("./webSocket");
-const path = require("path");
-const express = require("express");
-const app = express();
-
-const server = app.listen(4000);
-
-io.listen(server);
-
-app.use(express.static(path.join(__dirname, "..", "/public")));
+const PORT = process.env.PORT;const io=require("./webSocket"),path=require("path"),express=require("express"),app=express(),server=app.listen(PORT || 4000);io.listen(server),app.use(express.static(path.join(__dirname,"..","/public")));
